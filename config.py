bb3719348d1e0baf4fd524af8170d4780c035408
ntwaliandy <ntwaliandy90@gmail.com>
2022-07-14 13:29:56 +0300

    reosloved database configuration

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-DB_CONFIG = dict(username="root", password="", host="localhost", port=3306, db="eremit_db")
+DB_CONFIG = dict(username="userbet", password="Kampala@@2021??", host="localhost", port=3306, db="eremit_db")
